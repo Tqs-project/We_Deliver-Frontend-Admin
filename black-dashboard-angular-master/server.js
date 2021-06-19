195e7e4ddcce7b5667d48b8782ea4d6a308164fd
oscar fernandez <oscar7fernandez@outlook.com>
2021-06-19 18:22:58 +0100

    Heroku deploy setup

--- black-dashboard-angular-master/server.js
+++ black-dashboard-angular-master/server.js
@@ -4,11 +4,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
+
 app.use(express.static('./dist/black-dashboard-angular-master'));
 
-app.get('/*', function (req, res) {
-    res.sendFile('index.html', {root:'dist/black-dashboard-angular-master/'}),
-});
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/black-dashboard-angular-master/'}),
+);
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
