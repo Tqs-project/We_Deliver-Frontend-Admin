ab5255864b5f61ea4e8be7fdfc2461532b16f251
Orlando-pt <orlando.macedo15@ua.pt>
2021-06-22 04:14:35 +0100

    deploy

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
-//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
 
 app.use(express.static('./dist'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/'}),
+  res.sendFile('index.html', {root: 'dist/'}),
 );
 
 // Start the app by listening on the default Heroku port
